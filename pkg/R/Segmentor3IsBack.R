29ee8919378e2df8e8bf755e56d980cb3a6c502a
alice <alice@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-07-09 06:46:13 +0000

    removed PhiFinder, added estimation of overdispersion in .R file
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/segmentor3isbac@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/Segmentor3IsBack.R
+++ pkg/R/Segmentor3IsBack.R
@@ -1,13 +1,12 @@
-Segmentor<- function(data=numeric(), file=character(), model=1, Kmax = 15, param = 0.15) UseMethod("Segmentor")
+Segmentor<- function(data=numeric(), model=1, Kmax = 15, theta = numeric(), m = numeric()) UseMethod("Segmentor")
 
-Segmentor.default <-function(data=numeric(), file=character(), model=1, Kmax = 15, param = 0.15)
+Segmentor.default <-function(data=numeric(), model=1, Kmax = 15, theta = numeric(), m = numeric())
 {
   if ((model!=1)&(model!=2)&(model!=3)&(model!=4))
     stop("Choose model=1 (Poisson), 2 (normal), 3 (Negative Binomial) or 4 (Normal-Variance)")
-  if ((length(data)==0)&(length(file)==0))
-    stop("Give me a vector of data to segment or a file to open")
-  if ((length(data)>0)&(length(file)>0))
-    stop("Give me either a vector of data or a file to open but not both")
+  if (length(data)==0)
+    stop("Give me a vector of data to segment")
+
 
   breaks=matrix(0,nrow=Kmax,ncol=Kmax)
   breaks = as.vector(breaks)
@@ -15,20 +14,43 @@ Segmentor.default <-function(data=numeric(), file=character(), model=1, Kmax = 1
   parameters=as.vector(parameters)
   likelihood=rep(0, Kmax)
   likelihood=as.vector(likelihood)
-  n = 0    
-  if (length(file)>0)
-  {
-	data<-read.table(file)
-	data<-data[,1]
-  }
   n = length(data)
-  if ((model==4) & (param==0.15))
-	param = mean(data)
+  if ((model==4) & (length(m)==0))
+	m = mean(data)
+  if ((model==3) & (length(theta)==0))
+  {
+    i<-1
+    pold<-0
+    while (i<n)
+    {
+      pnew=0
+      while((i<n) & (data[i]!=0)) i<-i+1
+      while((i<n) & (data[i]==0) )
+      { 
+        pnew=pnew+1
+        i<-i+1
+      }
+      if(pnew>pold)
+      {
+        pold=pnew
+      }  
+    }
+    h<-max(2*pold,15)
+	Xcum = cumsum(data)
+	X2cum = cumsum(data^2)
+	M = (Xcum[h:n] - c(0, Xcum[1:(n-h)])) / h
+	S2 = (X2cum[h:n] - c(0, X2cum[1:(n-h)])) / (h-1) - h/(h-1)*M^2
+	P = (S2/M) - 1
+	K = M^2 / (S2-M)
+	W = h*(P/(1+P))^2
+	W = W / sum(W)
+    theta = median(K)
+  }
   if (model==1)
 	Rep<-.C("SegmentPoisson", Size = as.integer(n),KMax = as.integer(Kmax), Data = as.integer(data), Breakpoints = as.integer(breaks), Parameters = as.double(parameters), Likelihood = as.double(likelihood), PACKAGE="Segmentor3IsBack") else if (model==3)
-	Rep<-.C("SegmentBinNeg", Size = as.integer(n),KMax = as.integer(Kmax), theta = as.double(param), Data = as.integer(data), Breakpoints = as.integer(breaks), Parameters = as.double(parameters), Likelihood = as.double(likelihood), PACKAGE="Segmentor3IsBack") else if (model==2)
+	Rep<-.C("SegmentBinNeg", Size = as.integer(n),KMax = as.integer(Kmax), theta = as.double(theta), Data = as.integer(data), Breakpoints = as.integer(breaks), Parameters = as.double(parameters), Likelihood = as.double(likelihood), PACKAGE="Segmentor3IsBack") else if (model==2)
 	Rep<-.C("SegmentNormal", Size = as.integer(n),KMax = as.integer(Kmax), Data = as.double(data), Breakpoints = as.integer(breaks), Parameters = as.double(parameters), Likelihood = as.double(likelihood), PACKAGE="Segmentor3IsBack") else if (model==4)
-	Rep<-.C("SegmentVariance", Size = as.integer(n),KMax = as.integer(Kmax), theta = as.double(param), Data = as.double(data), Breakpoints = as.integer(breaks), Parameters = as.double(parameters), Likelihood = as.double(likelihood), PACKAGE="Segmentor3IsBack")
+	Rep<-.C("SegmentVariance", Size = as.integer(n),KMax = as.integer(Kmax), mu = as.double(m), Data = as.double(data), Breakpoints = as.integer(breaks), Parameters = as.double(parameters), Likelihood = as.double(likelihood), PACKAGE="Segmentor3IsBack")
 
   breaks=matrix(Rep$Breakpoints,ncol=Kmax)
   breaks = t(breaks)
@@ -65,13 +87,13 @@ Segmentor.default <-function(data=numeric(), file=character(), model=1, Kmax = 1
   if (model==3) 
   {
       model.dist="Negative binomial"
-      Segmentor.res=list(model=model.dist,breaks=breaks,parameters=parameters,likelihood=likelihood)
+      Segmentor.res=list(model=model.dist,breaks=breaks,overdispersion=theta,parameters=parameters,likelihood=likelihood)
   }
   if (model==4) 
   {
       likelihood = likelihood + n/2*log(2*pi)
       model.dist="Variance Segmentation"
-      Segmentor.res=list(model=model.dist,breaks=breaks,parameters=parameters,likelihood=likelihood)
+      Segmentor.res=list(model=model.dist,breaks=breaks,mean=m,parameters=parameters,likelihood=likelihood)
   }
   class(Segmentor.res) <- "Segmentor"
   Segmentor.res
@@ -88,7 +110,17 @@ print.Segmentor <-function(x,...)
 
   cat("\n Table of negative log-likelihood for each optimal segmentation: \n")
   print(x$likelihood)
-  
+
+  if (is.element("overdispersion",names(x)))
+  {
+    cat("\n Value of the overdispersion used for the segmentation: \n")
+    print(x$overdispersion)
+  }  
+  if (is.element("mean",names(x)))
+  {
+    cat("\n Value of the mean used for the segmentation: \n")
+    print(x$mean)
+  }  
   if (is.element("parameters",names(x)))
   {
     cat("\n Table of parameters: ")
